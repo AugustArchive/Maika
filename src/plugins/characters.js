cf6ce50ff32b228718afc30a7375cb0add872953
August (Chris) <augu@voided.pw>
2018-12-01 19:38:52 -0700

    unfinished: characters plugin

---
+++ src/plugins/characters.js
@@ -0,0 +1,13 @@
+const Plugin = require('../structures/plugin');
+
+module.exports = new Plugin({
+    name: 'Characters',
+    embeded: '::bust_in_silhouette: Anime Characters',
+    visible: true,
+    enabled: true,
+    commands: [
+        {
+            command: ''
+        }
+    ]
+});
