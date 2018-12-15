551625645bb4f0aa1e109660e5b1b7c79f569268
August (Chris) <augu@voided.pw>
2018-12-14 19:51:12 -0700

    fix(botlists): if the ID is Mafuyu (test bot)

--- rewrite/schedulers/botlists.js
+++ rewrite/schedulers/botlists.js
@@ -4,14 +4,20 @@ const Scheduler = require('../core/scheduler');
 module.exports = new Scheduler({
     name: 'botlists',
     interval: 60 * 1000 * 1000,
-    enabled: false,
+    enabled: true,
     run: async(bot) => {
+        // If the ID is Mafuyu
+        if (bot.user.id === '508842721545289731')
+            return;
+
         await request
             .post(`https://discordbots.org/api/bots/${bot.user.id}/stats`)
-            .set('Authorization', process.env.OLIY);
+            .set('Authorization', process.env.OLIY)
+            .send({ server_count: bot.guilds.size, shard_count: bot.shards.size })
         
         await request
             .post(`https://discordbotindex.com/apiv1/bot/${bot.user.id}`)
-            .set('Authorization', process.env.KIRB);
+            .set('Authorization', process.env.DERPY)
+            .send({ server_count: bot.guilds.size });
     }
 });
