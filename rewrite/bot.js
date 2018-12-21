f76fc867f1245f9ec9588b730826938b8c0a61ab
August (Chris) <augu@voided.pw>
2018-12-21 02:28:52 -0700

    meta(bot.js): UnhandledRejection message

--- rewrite/bot.js
+++ rewrite/bot.js
@@ -4,4 +4,4 @@ const MaikaClient = require('./core/client');
 const client = new MaikaClient();
 client.start();
 
-process.on('unhandledRejection', e => client.logger.error(`ehh? ${e.stack}`));
+process.on('unhandledRejection', e => client.logger.error(e.stack));
