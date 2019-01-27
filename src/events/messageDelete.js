1f9936d4ab3160c910d57a6de81e681e28bd4e38
August (Chris) <augu@voided.pw>
2019-01-27 00:39:28 -0700

    Create the snipe system.

---
+++ src/events/messageDelete.js
@@ -0,0 +1,22 @@
+const { Event } = require('../core');
+
+module.exports = class MessageDeletedEvent extends Event {
+    constructor(client) {
+        super(client, 'messageDelete');
+    }
+
+    run(message) {
+        if (!message.channel || !message.author)
+            return;
+
+        this
+            .client
+            .redis
+            .set(`deleted-${message.id}`, JSON.stringify({
+                author: message.author.toJSON(),
+                content: message.content,
+                timestamp: message.timestamp,
+                channel: message.channel
+            }), 'EX', 60 * 60);
+    }
+}
